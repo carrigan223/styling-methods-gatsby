01e28602c2d38c4c15dd6ebf372b88ebf1f5234e
Andrew Carrigan <carrigan223@gmail.com>
2020-12-15 11:48:02 -0800

    adding banner styling using modules

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,11 @@
 // src/components/Banner.js
 import React from "react"
+import bannerStyles from "../styles/modules/banner.module.css"
 
 export default function Banner() {
   return (
-    <section className="banner">
-      <h1 className="banner-title">
+    <section className={bannerStyles.banner}>
+      <h1 className={bannerStyles.bannerTitle}>
         This site uses cookies. We don't need your consent.
       </h1>
     </section>
