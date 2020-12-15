74196393f127797c31f60c7efcd6284963c3511c
Andrew Carrigan <carrigan223@gmail.com>
2020-12-15 13:50:25 -0800

    Using stylus

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,39 @@
 // src/components/Header.js
+import { Link } from "gatsby"
 import React from "react"
+import NavStyle from "../styles/stylus/header.module.styl"
+import "../styles/stylus/header.styl"
 import Container from "./Container"
 
 export default function Header() {
   return (
     <section id="nav">
       <Container>
-        <h1>Header</h1>
+        <nav className={NavStyle.navInner}>
+          <div className="nav-inner_start">
+            <h1 className="emoji">
+              <span role="img" aria-label="frown emoji">
+                🙁
+              </span>
+            </h1>
+          </div>
+          <div className="nav-inner_center">
+            <Link to="/" className="link">
+              Products
+            </Link>
+            <Link to="/" className="link">
+              Customers
+            </Link>
+            <Link to="/" className="link">
+              Pricing
+            </Link>
+          </div>
+          <div className="nav-inner_end">
+            <Link to="/" className={NavStyle.cta}>
+              GET STARTED
+            </Link>
+          </div>
+        </nav>
       </Container>
     </section>
   )
