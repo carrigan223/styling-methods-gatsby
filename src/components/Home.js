a5107d820ce99160584253f90073e833838070ff
Andrew Carrigan <carrigan223@gmail.com>
2020-12-16 00:21:35 -0800

    added scss module

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,27 @@
 // src/components/Home.js
 import React from "react"
+import HomeStyles from "../styles/scss/home.module.scss"
+import "../styles/scss/home.scss"
 import Container from "./Container"
 
 export default function Home() {
   return (
     <section id="home">
       <Container>
-        <h1>Home</h1>
+        <div className="home-inner">
+          <div className="home-inner_start">
+            <h1>DEAR LOREM IPSUM</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa
+              dolorum et voluptatibus itaque! Perferendis illo commodi, ad dolor
+              dolorum alias, nesciunt inventore suscipit sit consequuntur
+              tenetur dolorem minus natus dolores.
+            </p>
+          </div>
+          <div className="home-inner_end">
+            <div className={HomeStyles.box}></div>
+          </div>
+        </div>
       </Container>
     </section>
   )
